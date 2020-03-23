649daaf73e64492b1ac785193a75a1293715df03
Mahabub Hossain <you@example.com>
2020-03-23 15:00:58 +0600

    webbpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>Gettting started with webpack manually!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
